776c9ff43833b534ddf207ec8f138b05344a4188
James W. Fawcett <jfawcett@twcny.rr.com>
2020-04-30 19:50:41 -0400

    added IndexOutOfBounds

---
+++ IndexOutOfBounds/src/main.rs
@@ -0,0 +1,17 @@
+/////////////////////////////////////////////////////////////
+// index_out_of_bounds::main.rs                            //
+//                                                         //
+// Jim Fawcett, https://JimFawcett.github.io, 30 Apr 2020  //
+/////////////////////////////////////////////////////////////
+ 
+use std::io::*;
+
+fn main() {
+    let array = [1, 2, 3];
+    print!("\n  ");
+    for i in 0..4 {
+        let _ = std::io::stdout().flush();
+        print!("{} ", array[i]);
+    }
+    println!("\n  That's all Folks!\n");
+}
